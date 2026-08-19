431047fd6129c083301a073a186559757652ff85
BeeeY <beilinyao544@gmail.com>
2026-08-19 18:26:00 -0500

    Implemented glide

--- ladybug.gd
+++ ladybug.gd
@@ -1,24 +1,28 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+const SPEED = 150.0
+const JUMP_VELOCITY = -300.0
 
 var glide = false
+var glide_gravity
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor() and glide == false:
 		velocity += get_gravity() * delta
 		if Input.is_action_just_pressed("jump"):
+			$AnimatedSprite2D.play("Glide")
 			glide = true
-			
-	if is_on_floor():
-		glide = false
-		
+
 	if glide == true:
-		velocity += get_gravity() * delta * 0.3
-		
+		glide_gravity = get_gravity() * 0.2
+		velocity += glide_gravity * delta
+
+	if is_on_floor():
+		$AnimatedSprite2D.play("Walk")
+		glide = false
+
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
@@ -27,5 +31,14 @@ func _physics_process(delta: float) -> void:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+	
+	if direction > 0:
+		$AnimatedSprite2D.play()
+		$AnimatedSprite2D.flip_h = false
+	elif direction < 0:
+		$AnimatedSprite2D.play()
+		$AnimatedSprite2D.flip_h = true
+	elif direction == 0:
+		$AnimatedSprite2D.stop()
+		
 	move_and_slide()
