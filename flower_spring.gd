6d990838bc726a0d0b710f5b483422b21c9da2ee
BeeeY <beilinyao544@gmail.com>
2026-08-19 19:22:49 -0500

    Added flower spring

---
+++ flower_spring.gd
@@ -0,0 +1,24 @@
+extends Node2D
+
+signal flower_sprung
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	flower_sprung.emit()
+	$AnimatedSprite2D.animation = "PartlySprung"
+	await get_tree().create_timer(0.2).timeout
+	$AnimatedSprite2D.animation = "Sprung"
+	await get_tree().create_timer(0.3).timeout
+	$AnimatedSprite2D.animation = "PartlySprung"
+	await get_tree().create_timer(0.2).timeout
+	$AnimatedSprite2D.animation = "Unsprung"
+	
--- ladybug.gd
+++ ladybug.gd
@@ -1,6 +1,5 @@
 extends CharacterBody2D
 
-
 const SPEED = 150.0
 const JUMP_VELOCITY = -300.0
 
@@ -11,13 +10,15 @@ func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor() and glide == false:
 		velocity += get_gravity() * delta
+
 		if Input.is_action_just_pressed("jump"):
 			$AnimatedSprite2D.play("Glide")
 			glide = true
 
 	if glide == true:
-		glide_gravity = get_gravity() * 0.2
+		glide_gravity = get_gravity() * 0.1
 		velocity += glide_gravity * delta
+
 
 	if is_on_floor():
 		$AnimatedSprite2D.play("Walk")
@@ -35,10 +36,18 @@ func _physics_process(delta: float) -> void:
 	if direction > 0:
 		$AnimatedSprite2D.play()
 		$AnimatedSprite2D.flip_h = false
+		
 	elif direction < 0:
 		$AnimatedSprite2D.play()
 		$AnimatedSprite2D.flip_h = true
+		
 	elif direction == 0:
-		$AnimatedSprite2D.stop()
+		if glide == false:
+			$AnimatedSprite2D.stop()
 		
 	move_and_slide()
+
+
+func _on_flower_sprung() -> void:
+	velocity.y = -500
+	
